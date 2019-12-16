bc81b51b3db7f8b0ef6c746d5cc6130d4e48baf8
niyu <yuni@pateo.com.cn>
2019-12-16 13:05:49 +0800

    文件管理器

--- android/app/src/main/java/com/example/orange_player/MainActivity.java
+++ android/app/src/main/java/com/example/orange_player/MainActivity.java
@@ -1,13 +1,67 @@
 package com.example.orange_player;
 
 //import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.MimeTypeMap;
+
+import io.flutter.Log;
+import io.flutter.app.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.embedding.engine.plugins.PluginRegistry;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.EventChannel;
+import io.flutter.plugin.common.MethodCall;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  public void configureFlutterEngine(FlutterEngine flutterEngine) {
-    GeneratedPluginRegistrant.registerWith(flutterEngine);
-  }
+    private static Context mContext = null;
+    private static final String METHOD_CHANNEL = "openFileChannel";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+        mContext = this;
+        new MethodChannel(getFlutterView(), METHOD_CHANNEL).setMethodCallHandler(
+                (methodCall, result) -> {
+                    if (methodCall.method.equals("openFile")) {
+                        String path = methodCall.argument("path");
+                        openFile(mContext, path);
+                        result.success("");
+                    } else {
+                        result.notImplemented();
+                    }
+                }
+        );
+    }
+
+    private void openFile(Context context, String path) {
+        Log.d("NYDBG", "path = " + path);
+        try {
+            if (!path.contains("file://")) {
+                path = "file://" + path;
+            }
+            //获取文件类型
+            String[] nameType = path.split("\\.");
+            String mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(nameType[1]);
+
+            Intent intent = new Intent();
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setAction(Intent.ACTION_VIEW);
+            //设置文件的路径和文件类型
+            intent.setDataAndType(Uri.parse(path), mimeType);
+            //跳转
+            context.startActivity(intent);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,38 @@
 import 'dart:io';
+import 'dart:async';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:orange_player/file_manager.dart';
 import 'package:orange_player/secondscreen.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:orange_player/common.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() => runApp(MyApp());
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  Future<void> getSDCardDir() async {
+    Common().sDCardDir = (await getExternalStorageDirectory()).path;
+  }
+
+  // Permission check
+  Future<void> getPermission() async {
+    if (Platform.isAndroid) {
+      PermissionStatus permission = await PermissionHandler().checkPermissionStatus(PermissionGroup.storage);
+      if (permission != PermissionStatus.granted) {
+        await PermissionHandler().requestPermissions([PermissionGroup.storage]);
+      }
+      await getSDCardDir();
+    } else if (Platform.isIOS) {
+      await getSDCardDir();
+    }
+  }
+
+  Future.wait([initializeDateFormatting("zh_CN", null), getPermission()]).then((result) {
+    runApp(MyApp());
+  });
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -15,60 +42,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FileManager(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  File file;
-
-  @override
-  void initState() {
-    requestPermission();
-  }
-
-  void _getFile() async {
-    //    List<File> files = await FilePicker.getMultiFile();
-    file = await FilePicker.getFile();
-    Navigator.of(context).push(new MaterialPageRoute(builder: (_) {
-      return new SecondScreen(file);
-    }));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _getFile,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-  Future requestPermission() async {
-    // 申请权限
-    Map<PermissionGroup, PermissionStatus> permissions =
-        await PermissionHandler().requestPermissions(
-            [PermissionGroup.location, PermissionGroup.storage]);
-    // 申请结果
-    PermissionStatus permission = await PermissionHandler()
-        .checkPermissionStatus(PermissionGroup.location);
   }
 }
